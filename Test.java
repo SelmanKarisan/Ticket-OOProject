c8356e82523082f3bea6d644fb5f48507fd42acb
ahmet selman <karisanselman@gmail.com>
2022-04-23 00:18:20 +0300

    code --disable-extensions

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        Bus bus = new Bus(31, 31, "destination", "initialLocation", 31.31, 31);
-        
+
+        System.out.println("Marmara Turizm'e Hoşgeldiniz");
+        System.out.println("Bilet almak için: 1 \n Seyehat oluşturmak için: 2 ");
+
+    
+        //    Bus bus = new Bus(31, 31, "destination", "initialLocation", 31.31, 31);  
         // bus.generateSeats(31);
         bus.printShemaOfSeats();
     }
