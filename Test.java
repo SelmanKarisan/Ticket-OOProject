05c2bfc97fd25b7ebc4d086b5a1f00c9953595a0
ahmet selman <karisanselman@gmail.com>
2022-04-23 01:05:33 +0300

    a

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
     private static Scanner scanner;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+         scanner = new Scanner(System.in);
         System.out.println("Marmara Turizm'e Hoşgeldiniz");
         System.out.println("Bilet almak için: 1 \n Seyehat oluşturmak için: 2 ");
         int Selection=scanner.nextInt();
@@ -17,16 +17,6 @@ public class Test {
             String initialLocationInput=scanner.next();
             System.out.println("Seyehat etmek istediğiniz aracı seçiniz:\n 1:Tren\n2:Uçak\n3:Otobüs");
             int SelectionforVehicleInput=scanner.nextInt();
-
-
-
-
-        
-
-
-
-
-
         }
         //seyehat ekleme sistemi için 
         else if(Selection==2){
@@ -35,23 +25,37 @@ public class Test {
             System.out.println("Şifrenizi giriniz:");
             String password=scanner.next();
 
+            // kullanıcı doğru giriş yaptığı zaman
+            while(true){
+                if(UserName.equals("admin_marmara")&&password.equals("1883")){
 
 
 
 
 
+                }else if(UserName.equals("admin_marmara") && !password.equals("1883")){
+    
+                System.out.println("Şifreniz yanlış lütfen tekrar deneyiniz");
+                System.out.println("Şifrenizi giriniz:");
+                 password=scanner.next();
+                }else if(!UserName.equals("admin_marmara") && password.equals("1883")){
+    
+    
+    
+                }else if(!UserName.equals("admin_marmara") && !password.equals("1883")){
+    
+    
+                }
 
-
-
-
-
+            }
+            
 
         }
     
         //    Bus bus = new Bus(31, 31, "destination", "initialLocation", 31.31, 31);  
         // bus.generateSeats(31);
             
-        scanner = new Scanner(System.in);
+
         int selection = scanner.nextInt();
 
         if (selection == 2) {
@@ -62,16 +66,17 @@ public class Test {
             int transitSelection = scanner.nextInt();
             switch (transitSelection) {
                 case 1:
-                    Airplane airplane = new Airplane(price, travelTime, destination, initialLocation, coefficentOfPrice,
-                            maxSeatCapacity);
+                 //   Airplane airplane = new Airplane(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                       //     maxSeatCapacity);
                     break;
                 case 2:
-                    Train train = new Train(price, travelTime, destination, initialLocation, coefficentOfPrice,
-                            maxSeatCapacity);
+                   // Train train = new Train(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                     //       maxSeatCapacity);
                     break;
                 case 3:
-                    Bus bus = new Bus(price, travelTime, destination, initialLocation, coefficentOfPrice,
-                            maxSeatCapacity);
+                    //Bus bus = new Bus(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                      //2
+                      //      maxSeatCapacity);
                     break;
             
                 default:
