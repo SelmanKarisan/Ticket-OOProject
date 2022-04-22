755c4ddb5447a4c58b7e3174e884b8b96ebbc640
Feyzullah Asıllıoğlu <feyzullahasillioglu@gmail.com>
2022-04-23 00:36:27 +0300

    a

--- Test.java
+++ Test.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Test {
+
+    private static Scanner scanner;
+
     public static void main(String[] args) {
-        Bus bus = new Bus(31, 31, "destination", "initialLocation", 31.31, 31);
+        scanner = new Scanner(System.in);
+        int selection = scanner.nextInt();
+
+        if (selection == 2) {
+            System.out.print("Hangi araç için sefer eklemek istiyorsunuz(1, 2 ya da 3'ü tuşlayın)");
+            System.out.println("1-Uçak");
+            System.out.println("2-Tren");
+            System.out.println("3-Otobüs");
+            int transitSelection = scanner.nextInt();
+            switch (transitSelection) {
+                case 1:
+                    Airplane airplane = new Airplane(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                            maxSeatCapacity);
+                    break;
+                case 2:
+                    Train train = new Train(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                            maxSeatCapacity);
+                    break;
+                case 3:
+                    Bus bus = new Bus(price, travelTime, destination, initialLocation, coefficentOfPrice,
+                            maxSeatCapacity);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
         
-        // bus.generateSeats(31);
-        bus.printShemaOfSeats();
     }
 }
